c2ebc51662e6ef30d5c7c3fd7074a460cda6e603
Ryan Gavenda <115602005+hgryaaan@users.noreply.github.com>
2024-09-12 15:41:40 -0300

    submit

---
+++ js/script.js
@@ -0,0 +1,52 @@
+$(document).ready(function() {
+    /*botão menu*/
+    $('#mobile_btn').on('click', function () {
+        $('#mobile_menu').toggleClass('active');
+        $('#mobile_btn').find('i').toggleClass('fa-x');
+    });
+
+    /*botões home*/
+    const $items = $('.home-item');
+    let currentIndex = 0;
+
+    function showItem(index) {
+        if ($(window).width() <= 1048) {
+            $items.hide(); 
+            $items.eq(index).show();
+        } else {
+            $items.show(); 
+        }
+    }
+
+    function checkWidth() {
+        if ($(window).width() <= 1048) {
+            showItem(currentIndex);
+            $('.controls').show(); 
+        } else {
+            $('.controls').hide(); 
+            $items.show(); 
+        }
+    }
+
+    checkWidth();
+
+    $('.fa-chevron-right').click(function() {
+        if ($(window).width() <= 1048) {
+            currentIndex = (currentIndex + 1) % $items.length; 
+            showItem(currentIndex);
+        }
+    });
+
+    $('.fa-chevron-left').click(function() {
+        if ($(window).width() <= 1048) {
+            currentIndex = (currentIndex - 1 + $items.length) % $items.length;
+            showItem(currentIndex);
+        }
+    });
+
+    $(window).resize(function() {
+        checkWidth();
+    });
+    
+    
+});
